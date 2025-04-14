c541c54b0829241f53d91d0110460a43b2f53441
Matteo Sartini <matteo.sartini26@gmail.com>
2025-04-14 13:26:29 +0200

    Added properties to Tile class

--- src/thing-implementation/environments/smart-city/Tile.ts
+++ src/thing-implementation/environments/smart-city/Tile.ts
@@ -1,5 +1,14 @@
+//This class models a tile inside the smart city simulation grid.
 export class Tile {
-    private x:number;
-    private y:number;
+
+    //Coordinates inside the simulation.
+    private x : number = 0;
+    private y : number = 0;
+
+    //List of the vehicles present in the tile.
+    private vehicles: string[] = [];
+
+    //List of the static objects in the tile.
+    private staticObjects : string[] = [];
 
 }
