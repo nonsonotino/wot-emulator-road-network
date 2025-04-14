df9e3dcc382859f164b36e8976d5bf2572139326
Matteo Sartini <matteo.sartini26@gmail.com>
2025-04-14 13:50:45 +0200

    Added basic city TD

--- src/thing-implementation/environments/smart-city/SmartCity.ts
+++ src/thing-implementation/environments/smart-city/SmartCity.ts
@@ -6,9 +6,27 @@ import { Thing } from "../../Thing";
 //and the interactions they may have with other members of the simulation.
 export class SmartCity extends Thing {
 
+    // Base structure of the city's TD
+    private static initBase: WoT.ExposedThingInit = {
+        "@context": "https://www.w3.org/2019/wot/td/v1",
+        "@type": "Environment",
+        "description": "An environment representing a smart city's road network.",
+        "forms": [
+            {
+                "href": "environment",
+                "op": ["readproperty", "writeproperty", "observeproperty"],
+                "contentType": "application/json"
+            }
+        ],
+        "properties": {
+            
+        },
+        "events": {
+            
+        }
+    };
     
     public update(deltaTime: number): void {
         throw new Error("Method not implemented.");
     }
-    
 }
