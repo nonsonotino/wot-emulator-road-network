7265d1d7d5cb429b7d4a9542b0e5a20c417495b3
Matteo Sartini <matteo.sartini26@gmail.com>
2025-04-14 11:47:01 +0200

    Added properties to CityThing and fixed descriptions

--- src/thing-implementation/things/smart-city/CityThing.ts
+++ src/thing-implementation/things/smart-city/CityThing.ts
@@ -2,9 +2,15 @@ import Servient from "@node-wot/core";
 import { SmartCity } from "../../environments/smart-city/SmartCity";
 import { SituatedThing } from "../../SituatedThing";
 
-// Define an abstract class that extends SituatedThing. This will serve as a base class for all things in the museum.
-export abstract class MuseumThing extends SituatedThing<SmartCity> {
+// Defines an abstract class that extends SituatedThing.
+// This class represents any object inside the smart city.
+export abstract class CityThing extends SituatedThing<SmartCity> {
 
-    // The ID of the room where the object is located
-    protected roomId : string = '';
+    //Position inside the simulation grid
+    protected x : number = 0;
+    protected y : number = 0;
+
+    //Boolean to define if the object is static or can
+    //be moved inside the simulation grid.
+    protected movable : boolean = false;
 }
