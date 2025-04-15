425c5b654e382572d0f9e41e4ea879d053d935e0
Matteo Sartini <matteo.sartini26@gmail.com>
2025-04-15 14:19:39 +0200

    Updated simulation paths

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { inizializeServer } from "./simulation/server";
 
 // Path to the main configuration file for the simulator, defining the setup for servients, environments, and things.
-export const CONFIG = "./src/configuration/configMuseum.json";
+export const CONFIG = "./src/configuration/configSmartCity.json";
 
 // Path to the directory containing models for the various SituatedThings available in the simulation.
-export const THING_MODEL = "../thing-implementation/things/museum/";
+export const THING_MODEL = "../thing-implementation/things/smart-city/";
 
 // Path to the directory containing models for the different environments used in the simulation.
-export const ENV_MODEL = "../thing-implementation/environments/museum/";
+export const ENV_MODEL = "../thing-implementation/environments/smart-city/";
 
 inizializeServer();
 
