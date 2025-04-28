065026af74af0ce0d0a64b2b293629594ad89dae
Matteo Sartini <matteo.sartini26@gmail.com>
2025-04-28 20:25:34 +0200

    Updated Car structure

--- src/thing-implementation/things/smart-city/Car.ts
+++ src/thing-implementation/things/smart-city/Car.ts
@@ -7,47 +7,56 @@ import { write } from "fs";
 //It need to update its position moving in a new road cell.
 class Car extends CityThing {
 
-    private licensePlate : string = "";
+    private licensePlate: string = "";
 
-        // Base structure of the car's TD
-        private static initBase : WoT.ExposedThingInit = {
-            description: "Representation of a veichle moving inside the road network.",
-            forms: [ //TODO check correttezza form
-                {
-                    href: "things",  
-                    op: ["readproperty", "writeproperty", "observeproperty"],
-                    contentType: "application/json"
-                }
-            ],
-            properties: {
-                licensePlate: {
-                    type: "string",
-                    description: "Unique alphanumeric identification of a veichle in the simulation.",
-                    observable: true,
-                    readOnly: true,
-                    witeOnly: false,
-                    forms: [
-                        {
-                            href: "licensePlate",  
-                            op: ["readproperty"],
-                            contentType: "application/json"
-                        }
-                    ]
-                }
-            },
-            actions: {
-                move: {//TODO: non mi serve, tengo solo per riferimento
-                    description: "Moves the car in the next cell in its path.",
-                    forms: [
-                        {
-                            href: "move",  
-                            op: ["invokeaction"]
-                        }
-                    ]
-                }
+    //Base structure of the car's TD
+    private static initBase: WoT.ExposedThingInit = {
+        description: "Representation of a vehicle moving inside the road network.",
+        forms: [ //TODO check correttezza form
+            {
+                href: "things",
+                op: ["readproperty", "writeproperty", "observeproperty"],
+                contentType: "application/json"
             }
-        };
-    
+        ],
+        properties: {
+            licensePlate: {
+                type: "string",
+                description: "Unique alphanumeric identification of a vehicle in the simulation.",
+                observable: true,
+                readOnly: true,
+                witeOnly: false,
+                forms: [
+                    {
+                        href: "licensePlate",
+                        op: ["readproperty"],
+                        contentType: "application/json"
+                    }
+                ]
+            }
+        },
+        actions: {
+            move: {//TODO: non mi serve, tengo solo per riferimento
+                description: "Moves the car in the next cell in its path.",
+                forms: [
+                    {
+                        href: "move",
+                        op: ["invokeaction"]
+                    }
+                ]
+            }
+        }
+    };
+
+    //Returns the car's liecense plate.
+    public getLicensePlate(): string {
+        return this.licensePlate;
+    }
+
+    private move(): void {
+        
+    }
+
     //The car moves in a random direction from those available
     //updates its own position and the position in the envoronment.
     public update(deltaTime: number): void {
