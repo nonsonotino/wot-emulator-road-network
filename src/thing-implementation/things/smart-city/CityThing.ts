95fa0a292b7968a8a3d3e9d4834a61d47c7b17cd
Matteo Sartini - matteo.sartini8@studio.unibo.it <matteo.sartini8@studio.unibo.it>
2025-05-05 16:59:32 +0200

    Deleted movable property from CityThing

--- src/thing-implementation/things/smart-city/CityThing.ts
+++ src/thing-implementation/things/smart-city/CityThing.ts
@@ -9,8 +9,5 @@ export abstract class CityThing extends SituatedThing<SmartCity> {
 
     //Position inside the simulation grid
     protected coords: Coordinate = { x: 0, y: 0 };  
-
-    //Boolean to define if the object is static or can
-    //be moved inside the simulation grid.
-    protected movable: boolean = true; //TODO: is it needed?
+    
 }
