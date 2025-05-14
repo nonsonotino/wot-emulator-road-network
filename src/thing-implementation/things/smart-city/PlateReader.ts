d44447565c9909a9792c00ec54a9e84e82a0d846
Matteo Sartini - matteo.sartini8@studio.unibo.it <matteo.sartini8@studio.unibo.it>
2025-05-14 17:15:11 +0200

    Added history to license plate reader

--- src/thing-implementation/things/smart-city/PlateReader.ts
+++ src/thing-implementation/things/smart-city/PlateReader.ts
@@ -1,8 +1,18 @@
 import Servient from "@node-wot/core";
 import { SmartCity } from "../../environments/smart-city/SmartCity";
 import { CityThing, PeriodicCityThing } from "./CityThing";
+import { title } from "process";
+
+//Type representing the event emitted by the plate reader. 
+export type ReaderEvent  = {
+    licencePlate: string,
+    timestamp: Date,
+};
 
 export class PlateReader extends CityThing {
+
+    //Plate reader history.
+    private history: ReaderEvent[] = [];
 
     //Base structure of the plate reader's TD.
     private static initBase: WoT.ExposedThingInit = {
@@ -45,9 +55,25 @@ export class PlateReader extends CityThing {
         this.environment.addPlateReader(this);
     }
 
+    //Return the history of the plate reader.
+    public getHistory(): ReaderEvent[] {
+        return this.history;
+    }
+
     //Plate reader update method.
     public update(deltaTime: number): void {
 
+    }
+
+    //Retutn the JSON representation of the car.
+    public toString(): string {
+        return JSON.stringify({
+            title: this.getTitle(),
+            type: this.constructor.name,
+            id: this.getId(),
+            coordinates: this.getCoordinates(),
+            history: this.getHistory()
+        });
     }
 }
 
